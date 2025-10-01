3d1238e983f7f66aa649618684332a3b243689b3
ashokadi34 <ashokadi34@gmail.com>
2025-10-01 10:08:22 +0530

    file added

---
+++ src/test/java/seleniumJavaCode/AUTOMATION_PRACTICE_WEBSITES.java
@@ -0,0 +1,22 @@
+package seleniumJavaCode;
+
+public class AUTOMATION_PRACTICE_WEBSITES {
+
+	public static void main(String[] args) {
+		
+			/*
+			 * Top Platforms for QA Automation Practice:
+			 • The Internet Herokuapp: https://lnkd.in/dCHeAsQR
+			 • UI Testing Playground: https://lnkd.in/dfNVbw9A
+			 • The Playground: https://lnkd.in/dbRYd2qv
+
+			Best Demo Sites Featuring E-Commerce and Banking:
+			 • ParaBank: https://lnkd.in/dmvrMPue
+			 • Luma - Magento eCommerce: https://lnkd.in/dr3tCdse
+			 */
+		
+		System.out.println("AUTOMATION_PRACTICE_WEBSITES");
+
+	}
+
+}
