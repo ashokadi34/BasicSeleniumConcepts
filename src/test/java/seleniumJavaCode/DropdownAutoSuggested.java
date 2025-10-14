695db8f88f9af4707af4523cf1ffa48b5a69d972
ashokadi34 <ashokadi34@gmail.com>
2025-10-14 23:25:25 +0530

    new file added

---
+++ src/test/java/seleniumJavaCode/DropdownAutoSuggested.java
@@ -0,0 +1,42 @@
+package seleniumJavaCode;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DropdownAutoSuggested {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20)); //implicit wait
+		driver.get("http://google.com/");
+		//driver.get("https://www.bjs.com/"); //bjs
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.name("q")).sendKeys("selenium");
+		//driver.findElement(By.name("search")).sendKeys("mobile"); //bjs
+		Thread.sleep(5000);
+		
+		List<WebElement> list = driver.findElements(By.xpath("//ul[@role='listbox']//li//div[@role='option']"));  //google
+		//driver.findElement(By.xpath("//span[contains(.,'mobile phones')]")).click(); //bjs
+		//List<WebElement> list = driver.findElements(By.xpath("//*[contains(@class,'list list-group')]")); //bjs
+		System.out.println(list.size());
+		
+		for(int i=0; i<list.size();i++)
+		{
+			System.out.println(list.get(i).getText());
+			if (list.get(i).getText().equals("selenium"))
+			{
+				list.get(i).click();
+				break;
+			}
+		}
+		
+	}
+
+}
