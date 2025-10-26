47bafbd3b1d886512af1b0fa3a9d0f606c2c0de8
ashokadi34 <ashokadi34@gmail.com>
2025-10-26 11:36:38 +0530

    new file added

---
+++ src/test/java/seleniumJavaCode/GetConditionalMethods.java
@@ -0,0 +1,36 @@
+package seleniumJavaCode;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GetConditionalMethods {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); //implicit wait
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().window().maximize();		
+		
+//		driver.findElement(By.xpath("//a[normalize-space()='OrangeHRM, Inc']")).click();
+		
+		System.out.println("current window url : "+driver.getCurrentUrl());
+		System.out.println("current window title : "+driver.getTitle());
+//		System.out.println(driver.getPageSource());
+//		
+//		System.out.println("window handle : "+driver.getWindowHandle());
+		System.out.println("window handles : "+driver.getWindowHandles());
+		
+		Thread.sleep(3000);
+		
+		System.out.println(driver.findElement(By.xpath("//img[@alt='company-branding']")).isDisplayed());
+		System.out.println(driver.findElement(By.xpath("//input[@placeholder='Username']")).isEnabled());
+		
+						
+		driver.quit();
+	}
+
+}
