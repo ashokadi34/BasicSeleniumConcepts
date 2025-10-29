9ef2f5a0d1f6fd5b1525f74c5dd3c8a52bf15e5b
ashokadi34 <ashokadi34@gmail.com>
2025-10-29 20:07:37 +0530

    new file added

---
+++ src/test/java/seleniumJavaCode/JsExecutorClickSendkeys.java
@@ -0,0 +1,34 @@
+package seleniumJavaCode;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JsExecutorClickSendkeys {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("https://testautomationpractice.blogspot.com/");
+		driver.manage().window().maximize();
+		
+		WebElement input=driver.findElement(By.xpath("//input[@id='name']"));
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		//js.executeScript("arguments[0].setAttribute('value','JohnDeer')", input);
+		js.executeScript("arguments[0].value='New Value'", input);
+		WebElement radio=driver.findElement(By.xpath("//input[@id='male']"));
+		js.executeScript("arguments[0].click()", radio);
+		
+//		String toDisable = "document.getElementById('email').setAttribute('disabled', '');";
+		js.executeScript("document.getElementById('email').setAttribute('disabled', '');");
+		Thread.sleep(2000);
+		js.executeScript("document.getElementById('email').removeAttribute('disabled', );");
+
+	}
+
+}
