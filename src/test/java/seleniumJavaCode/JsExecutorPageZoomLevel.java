1c802268bc7c7ada29f3919c086e13bd800dfb1f
ashokadi34 <ashokadi34@gmail.com>
2025-10-30 19:56:17 +0530

    new file added

---
+++ src/test/java/seleniumJavaCode/JsExecutorPageZoomLevel.java
@@ -0,0 +1,25 @@
+package seleniumJavaCode;
+
+import java.time.Duration;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JsExecutorPageZoomLevel {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("https://testautomationpractice.blogspot.com/");
+		driver.manage().window().maximize();
+		
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("document.body.style.zoom='50%'","");
+		Thread.sleep(3000);
+		js.executeScript("document.body.style.zoom='80%'", "");
+		
+	}
+
+}
