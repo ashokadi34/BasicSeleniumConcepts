3a017a51a5d3ef636643e4385a97e82b0492fa31
ashokadi34 <ashokadi34@gmail.com>
2025-11-19 21:55:11 +0530

    done small changes

--- src/test/java/seleniumJavaCode/SwitchBrowserWindows.java
+++ src/test/java/seleniumJavaCode/SwitchBrowserWindows.java
@@ -37,7 +37,7 @@ public class SwitchBrowserWindows {
 		for (String winID : windowsList)
 		{
 			String title = driver.switchTo().window(winID).getTitle();
-			System.out.println(title);
+			System.out.println(title);	//print the title
 			
 //			if (title.equals("OrangeHRM"))
 //			{
